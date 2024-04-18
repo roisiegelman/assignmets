768ef6ba78c4f27eaae19a483f79c62067462af4
Roi Siegelman <roi.siegelman@weizmann.ac.il>
2024-04-18 12:46:48 +0300

    rm cal.py

--- cal.py
+++
@@ -1,9 +0,0 @@
-# length=input("length: ")
-# width= input("width: ")
-first = input("first number:")
-second = input("second number:")
-sum = first + second
-print(f"The sum is {sum} ")
-
-sume = float(first) + float(second)
-print(f"The sum is {sume} ")
