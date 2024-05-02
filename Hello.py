2b5446fa56258337a7d00c046426163aafb0c1e0
Roi Siegelman <roi.siegelman@weizmann.ac.il>
2024-05-02 10:00:59 +0300

    add .

---
+++ Hello.py
@@ -0,0 +1,4 @@
+print("Hello World")
+First_Name = input("What is your name?")
+Last__Name = input("What is your last name?")
+print(First_Name + ' ' + Last__Name)
---
+++ Logical_operators.py
@@ -0,0 +1,27 @@
+a = 5
+b = 6
+
+print((a > 2) and (b >= 6))    # True
+
+# logical AND
+print(True and True)     # True
+print(True and False)    # False
+
+# logical OR
+print(True or False)     # True
+
+# logical NOT
+print(not True)          # False
+
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+
+print(x1 is not y1)  # prints False
+
+print(x2 is y2)  # prints True
+
+print(x3 is y3)  # prints False
