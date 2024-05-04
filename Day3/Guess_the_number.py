5c558e83f2d1e81d5a0cc810f51c42db333d55f9
Roi Siegelman <roi.siegelman@weizmann.ac.il>
2024-05-04 08:52:41 +0300

    add Guess_the_number.py

---
+++ Day3/Guess_the_number.py
@@ -0,0 +1,63 @@
+import random
+
+
+def generate_random_number(): 
+ return int(random.randint(1,20))
+
+def other_options():
+    return input("Guess the number between 1 and 20, or press 'x' to exit, 'n' for a new game, 's' to show the number): ")
+
+def play_game():
+    number_to_guess = generate_random_number()
+    play = 0 
+    
+    while True:
+     my_input = other_options()
+        
+     if my_input == 'x':
+            print("Exiting the program.")
+            return False
+     elif my_input == 'n':
+            print("Starting a new game.")
+            return True
+     elif my_input == 's':
+            print("The hidden number is:", number_to_guess)
+            continue
+        
+     try:
+            my_guess = int(my_input)
+     except ValueError:
+            print("Invalid input. Please enter a number between 1 and 20.")
+            continue
+        
+     play += 1
+     
+    
+     if my_guess == number_to_guess: 
+          print("Congratulations! You guessed the number {} correctly in {} guesses.".format(number_to_guess, play))
+          return True
+        
+     elif my_guess < number_to_guess:
+          print("Too small! Try again.")
+        
+     elif my_guess > number_to_guess:
+          print("Too big! Try again.")
+
+def main():
+    print("Welcome to the Number Guessing Game!")
+    
+    while True:
+        if not play_game():
+            break
+
+        play_again = input("\nDo you want to play again? (yes/no): ").lower()
+        while play_again not in ['yes', 'no']:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+            play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again == 'no':
+            print("Exiting the program.")
+            break
+
+if __name__ == "__main__":
+    main()
+  
